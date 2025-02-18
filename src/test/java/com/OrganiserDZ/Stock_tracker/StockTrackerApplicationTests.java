5d87948a92a0053a1f44a556cf145147cbf54e1b
BoualemChihaoui <bochihaoui@gmail.com>
2025-02-18 17:44:15 -0500

    initial commit

---
+++ src/test/java/com/OrganiserDZ/Stock_tracker/StockTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OrganiserDZ.Stock_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
