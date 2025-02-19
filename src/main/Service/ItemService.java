cbe31c27c781b193ccd7c383f0efc150ac745451
BoualemChihaoui <bochihaoui@gmail.com>
2025-02-18 20:46:42 -0500

    new update

--- src/main/Service/ItemService.java
+++ src/main/Service/ItemService.java
@@ -1,7 +1,14 @@
 
-import
+import java.util.List;
+import 
 
+@service
 
 public class ItemService {
+    privatefinal ItemRepository itemRepository;
+    public ItemService (ItemRepository itemRepository){
+        this.itemrepository=itemRepository;
+    }
+     public List<Item>
 
 }
